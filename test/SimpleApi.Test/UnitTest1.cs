3bac7b1ed9dabd98383b17162c9bfe527227ce7d
hamedur-rahiman <hamedur.rahiman@gmail.com>
2020-02-21 15:19:31 +0530

    Initial Commit

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnMyName()
+        {
+            var returnValue = controller.Get(1);
+
+            Assert.Equal("HR", returnValue.Value);
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
